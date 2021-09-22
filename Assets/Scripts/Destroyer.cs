827ba0d6c701f023ab87956661273fe40847e0d6
Muhammad Aulia Rahman <meehchannel7@gmail.com>
2021-09-22 17:00:20 +0700

    1.0
    Tugas 5

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        string tag = col.gameObject.tag;
+        if (tag == "Bird" || tag == "Enemy" || tag == "Obstacle")
+        {
+            Destroy(col.gameObject);
+        }
+            
+     }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
